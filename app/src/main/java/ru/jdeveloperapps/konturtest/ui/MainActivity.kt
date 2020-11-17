ab9d2ba66f636e66d4403dcd87f5d29e18d1e77c
Jvitaliy82 <jvit82@gmail.com>
2020-11-17 17:18:08 +0700

    add nav_graph

--- app/src/main/java/ru/jdeveloperapps/konturtest/ui/MainActivity.kt
+++ app/src/main/java/ru/jdeveloperapps/konturtest/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.jdeveloperapps.konturtest
+package ru.jdeveloperapps.konturtest.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.jdeveloperapps.konturtest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
